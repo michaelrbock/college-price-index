3f0d4c63622af91a776ba61f4fa21293ecc6efa5
Ze'ev <lailari.zeev@gmail.com>
2015-01-17 13:28:12 -0500

    added emoji to reegex match

--- parse.py
+++ parse.py
@@ -1,6 +1,34 @@
 import re, json, pdb
 
 # Things to match:
+
+# print u'[', u'\U0001F300-\U0001F5FF', u'\U0001F600-\U0001F64F',
+# 			u'\U0001F680-\U0001F6FF', u'\u2600-\u26FF\u2700-\u27BF]+'
+
+hamburger = u'\U0001F354'
+pizza = u'\U0001F355'
+chicken = u'\U0001F356'
+chicken1 = u'\U0001F357'
+sushi = u'\U0001F363'
+sushi2 = u'\U0000E344'
+bowling = u'\U0001F3B3'
+spaghetti = u'\U0001F35D'
+icecream = u'\U0001F366'
+icecream1 = u'\U0001F367'
+icecream2 = u'\U0001F368'
+shrooms = u'\U0001F344'
+cinRoll = u'\U0001F365'
+coffee = u'\U0001F375'
+drink = u'\U0001F375'
+drink1 = u'\U0001F376'
+drink2 = u'\U0001F377'
+drink3 = u'\U0001F378'
+drink4 = u'\U0001F379'
+drink4 = u'\U0001F37A'
+drink4 = u'\U0001F37B'
+
+
+
 keywords = {
     "meals": [
         "breakfast",
@@ -18,7 +46,21 @@ keywords = {
         "chipotle",
         "froyo",
         "sushi",
-        "fries"
+        "fries",
+        hamburger,
+		pizza    ,
+		chicken  ,
+		chicken1 ,
+		sushi    ,
+		sushi2   ,
+		bowling  ,
+		spaghetti,
+		icecream ,
+		icecream1,
+		icecream2,
+		shrooms  ,
+		cinRoll  ,
+		coffee   
     ],
     "housing" : [
         "rent",
@@ -30,7 +72,10 @@ keywords = {
         "ticket",
         "airbnb",
         "hotel",
-        "flight"
+        "flight",
+        "cab",
+        "taxi",
+        "lyft"
     ],
     "school" : [
         "school supplies",
@@ -43,7 +88,14 @@ keywords = {
         "tickets",
         "concert",
         "beer",
-        "weed"
+        "weed",
+        drink ,
+		drink1,
+		drink2,
+		drink3,
+		drink4,
+		drink4,
+		drink4
     ]
 }
 
@@ -97,7 +149,7 @@ def parseNote(note):
 
 def main():
 	# read file in to variable and make is readable as json
-	with open("testData.json", "r") as myfile:
+	with open("testData2.json", "r") as myfile:
 		data = json.load(myfile)
 	# call method to test
 	classifyPayment(data)
