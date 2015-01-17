295a7725d9f84b90cbecf56c336b2f0bf231de0f
Michael Bock <mykel.bock@gmail.com>
2015-01-17 03:39:18 -0500

    add parse to main

--- main.py
+++ main.py
@@ -4,9 +4,14 @@ import webapp2
 import jinja2
 import json
 import os
+import re
 import urllib
 from google.appengine.ext import ndb
 from google.appengine.api import urlfetch
+
+import sys
+sys.path.insert(0, os.path.abspath(os.path.dirname(__file__)))
+import dateutil.parser
 
 
 jinja_environment = jinja2.Environment(autoescape=True,
@@ -47,23 +52,107 @@ class OAuthStartHandler(BaseHandler):
             'response_type=token&redirect_uri={2}').format(client_id, scope, redirect_uri))
 
 
+# Things to match:
+keywords = [
+    "burger",
+    "cereal",
+    "rent",
+    "gas",
+    "bread",
+    "beer",
+    "weed",
+    "books",
+    "textbooks",
+    "dinner",
+    "pizza"
+]
+
+regexes = []
+
+def compileRegex():
+    # compile all the keywords as regex
+    for keyword in keywords:
+        regexes.append(re.compile(keyword, re.IGNORECASE))
+
+def parseNote(note):
+    item = ""
+    for index, regex in enumerate(regexes):
+        if regex.match(note):
+            # if description has 2 matching classifiers, toss it out
+            if item:
+                return False
+            item = keywords[index]
+    return item
+
+def classifyPayment(data):
+    compileRegex()
+
+    items = []
+
+    # only try to classify if payment went through
+    for index, payment in enumerate(data["data"]):
+        if payment["status"] == "settled":
+            amount = payment["amount"]
+            date = payment["date_created"]
+            note = payment["note"]
+
+            item = parseNote(note)
+
+            # send to db if regex matched value
+            if item:
+                items.append({'title': item, 'amount': amount, 'date': date, 'note': note})
+
+    return items
+
+
 class OAuthSuccessHandler(BaseHandler):
     def get(self):
         """Get access_token and then get payments and add to db all in one!"""
 
         access_token = self.request.get('access_token')
-        # self.write('success! ' + access_token + '\n')
 
         # make payments API call with access_token
         url = 'https://api.venmo.com/v1/payments?limit=1000&access_token=' + access_token
-        result = urlfetch.fetch(url)
+        response = urlfetch.fetch(url)
 
-        if result.status_code == 200:
-            self.write(result.content)
+
+        # parse json, create db entries
+        if response.status_code == 200:
+            items = classifyPayment(json.loads(response.content))
+        self.write(items)
+
+        for item in items:
+            item_entry = Item(date=dateutil.parser.parse(item['date']), title=item['title'],
+                amount=item['amount'], note=item['note'])
+            self.write(item_entry)
+            item_entry.put()
+
+
+class StatsHandler(BaseHandler):
+    def get(self):
+        self.write('stats')
+
+
+class Item(ndb.Model):
+    date = ndb.DateTimeProperty(required=True)
+    title = ndb.StringProperty(required=True)
+    amount = ndb.FloatProperty(required=True)
+    note = ndb.TextProperty(required=True)
+
+
+class Category(ndb.Model):
+    date_created = ndb.DateTimeProperty(auto_now_add=True)
+    title = ndb.StringProperty(required=True)
+
+
+class User(ndb.Model):
+    date_created = ndb.DateTimeProperty(auto_now_add=True)
+
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
     ('/signin/?', OAuthStartHandler),
-    ('/success/?', OAuthSuccessHandler)
+    ('/success/?', OAuthSuccessHandler),
+    ('/stats/?', StatsHandler)
 ], debug=True)
