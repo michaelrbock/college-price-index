a6b7c2e26f9d60b38fc88d114abc1c9f2ecb898f
Michael Bock <mykel.bock@gmail.com>
2015-01-17 00:37:02 -0500

    templates rendering

--- main.py
+++ main.py
@@ -1,24 +1,32 @@
 #!/usr/bin/env python
-#
-# Copyright 2007 Google Inc.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-import webapp2
 
-class MainHandler(webapp2.RequestHandler):
+import webapp2
+import jinja2
+import json
+import os
+from google.appengine.ext import ndb
+
+
+jinja_environment = jinja2.Environment(autoescape=True,
+    loader=jinja2.FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')))
+
+
+def render_str(template, **params):
+    t = jinja_environment.get_template(template)
+    return t.render(params)
+
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
+
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
+
+
+class MainHandler(BaseHandler):
     def get(self):
-        self.response.write('Hello world!')
+        self.render('index.html')
+
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
