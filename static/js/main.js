b6a89a30a88a934be894e10e5ef7b23306d8131e
Julian Ozen <julian.ozen@me.com>
2015-01-17 21:41:25 -0500

    working main graph and smaller graphs

--- static/js/main.js
+++ static/js/main.js
@@ -1,55 +1,38 @@
-var colors = {'food':'#fff', 'transportation':'#fff', 'school':'#fff', 'recreation':'#fff', 'vices':'#fff',
-'housing':'#fff'}
+var highcatagories = ['food','transportation','school','recreational','vices','housing']
+var colors = {'food':'#c16e33', 'transportation':'#3b489a', 'school':'#5b816c', 'recreational':'#694b81', 'vices':'#86963b',
+'housing':'#55302c'}
 
 
-var data = [4, 8, 15, 16, 23, 42];
-
-var width = $("#maxwidth").width(),
-    barHeight = 20;
-
-var x = d3.scale.linear()
-    .domain([0, d3.max(data)])
-    .range([0, width]);
-
-var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", barHeight * data.length);
-
-var bar = chart.selectAll("g")
-    .data(data)
-  .enter().append("g")
-    .attr("transform", function(d, i) { return "translate(0," + i * barHeight + ")"; });
-
-bar.append("rect")
-    .attr("width", x)
-    .attr("height", barHeight - 1);
-
-bar.append("text")
-    .attr("x", function(d) { return x(d) - 3; })
-    .attr("y", barHeight / 2)
-    .attr("dy", ".35em")
-    .text(function(d) { return d; });
+var maingraphdata = null;
+d3.json("static/src/data2.json", function(error, json) {
 
 
+    maingraphdata = json.data
 
 
-var n = 6, // number of layers
-    m = 200, // number of samples per layer
+var catindexmain = 0;
+
+
+var n = highcatagories.length, // number of layers
+    m = maingraphdata[0].history.length, // number of samples per layer
     stack = d3.layout.stack().offset("wiggle"),
     // stack = d3.layout.stack().offset("expand"),
-    // layers1 = stack([[{'x':0,'y':.2 },{'x':1,'y':.4},{'x':2,'y':.5},{'x':3,'y':.1}], [{'x':0,'y':.8 },{'x':1,'y':.6},{'x':2,'y':.5},{'x':3,'y':.9}]]);
+
     layers0 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
-    layers1 = stack(d3.range(n).map(function() { return bumpLayer(m); }));
+    console.log(layers0);
+
+
+
 
 var width = $("#mainImage").width(),
-    height = $("#mainImage").width()*.5;
+    height = $("#mainImage").width()*.25;
 
 var x = d3.scale.linear()
     .domain([0, m - 1])
     .range([0, width]);
 
 var y = d3.scale.linear()
-    .domain([0, d3.max(layers0.concat(layers1), function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
+    .domain([0, d3.max(layers0, function(layer) { return d3.max(layer, function(d) { return d.y0 + d.y; }); })])
     .range([height, 0]);
 
 var color = d3.scale.linear()
@@ -70,37 +53,156 @@ svg.selectAll("path")
     .attr("d", area)
     .style("fill", function() { return color(Math.random()); });
 
-function transition() {
-  d3.selectAll("path")
-      .data(function() {
-        var d = layers1;
-        layers1 = layers0;
-        return layers0 = d;
-      })
-    .transition()
-      .duration(2500)
-      .attr("d", area);
-}
 
 // Inspired by Lee Byron's test data generator.
 function bumpLayer(n) {
 
-  function bump(a) {
-    var x = 1 / (.1 + Math.random()),
-        y = 2 * Math.random() - .5,
-        z = 10 / (.1 + Math.random());
-    for (var i = 0; i < n; i++) {
-      var w = (i / n - y) * z;
-      a[i] += x * Math.exp(-w * w);
+
+
+      function bump(a) {
+        var x = 1 / (.1 + Math.random()),
+            y = 2 * Math.random() - .5,
+            z = 10 / (.1 + Math.random());
+        for (var i = 0; i < n; i++) {
+          var w = (i / n - y) * z;
+          a[i] += x * Math.exp(-w * w);
+        }
+      }
+
+      var a = [], i;
+      mainDataDict = maingraphdata[catindexmain];
+      console.log(mainDataDict)
+
+
+      // console.log(a,i)
+      for (i = 0; i < n; ++i) a[i] = maingraphdata[catindexmain].history[i].average;
+
+
+
+
+    // return [{'x':0,'y':.2 },{'x':1,'y':.3},{'x':2,'y':.5},{'x':3,'y':.5}]
+      results= a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
+      catindexmain += 1;
+      return results
     }
-  }
-
-  var a = [], i;
-  for (i = 0; i < n; ++i) a[i] = 0;
-  for (i = 0; i < 5; ++i) bump(a);
 
 
 
-// return [{'x':0,'y':.2 },{'x':1,'y':.3},{'x':2,'y':.5},{'x':3,'y':.5}]
-  return a.map(function(d, i) { return {x: i, y: Math.max(0, d)}; });
-}
+});
+
+
+
+
+
+highcatagories.forEach(function(catagory){
+
+    var catagoryid = "#" + catagory + "Chart";
+    // console.log(catagoryid)
+
+
+
+    var format = d3.time.format("%m/%d/%Y");
+
+    var margin = {top: 20, right: 30, bottom: 30, left: 40},
+        width = $(catagoryid).width() - margin.left - margin.right,
+        height = $(catagoryid).width() - margin.top - margin.bottom;
+        // height = height /2;
+
+    var x = d3.time.scale()
+        .range([0, width]);
+
+    var y = d3.scale.linear()
+        .range([height, 0]);
+
+    var z = d3.scale.category20c();
+
+    var xAxis = d3.svg.axis()
+        .scale(x)
+        .orient("bottom")
+        .ticks(d3.time.months, 6);
+
+    var yAxis = d3.svg.axis()
+        .scale(y)
+        .tickFormat(d3.format("$s"))
+        .orient("left");
+
+    var stack = d3.layout.stack()
+        .offset("zero")
+        .values(function(d) { return d.values; })
+        .x(function(d) { return d.date; })
+        .y(function(d) { return d.value; });
+
+    var nest = d3.nest()
+        .key(function(d) { return d.key; });
+
+    var area = d3.svg.area()
+        .interpolate("basis")
+
+        .x(function(d) { return x(d.date); })
+        .y0(function(d) { return y(d.y0); })
+        .y1(function(d) { return y(d.y0 + d.y); });
+
+    var svg = d3.select(catagoryid).append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+      .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+
+
+    d3.json("static/src/data.json", function(error, json) {
+          // if (error) return console.warn(error);
+          // console.log(json)
+        var catindex = 0
+        var index = 0;
+        json.data.forEach(function(d){
+
+
+            if (d.title == catagory) {
+                catindex = index
+
+            }
+            index = index +1;
+
+          });
+
+          data = json.data[catindex].history
+
+          data.forEach(function(d){
+
+                d.date = format.parse(d.start_date);
+
+                d.value = +d.average;
+
+
+
+
+            })
+
+
+
+
+
+      var layers = stack(nest.entries(data));
+
+      x.domain(d3.extent(data, function(d) { return d.date; }));
+      y.domain([0, d3.max(data, function(d) { return d.y0 + d.y; })]);
+
+      svg.selectAll(".layer")
+          .data(layers)
+        .enter().append("path")
+          .attr("class", "layer")
+          .attr("d", function(d) { return area(d.values); })
+          .style("fill", colors[catagory]);
+
+      svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+      svg.append("g")
+          .attr("class", "y axis")
+           .attr("transform", "translate(5,0)")
+          .call(yAxis);
+    });
+});
